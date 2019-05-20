ca013b822a24bc56234d178e5e3d4bc41fa1bb2c
Tijs-B <tijs.bergmans@student.kuleuven.be>
2019-05-20 17:36:04 +0200

    let users move charging reservations

--- reservation/static/reservation/utils.js
+++ reservation/static/reservation/utils.js
@@ -23,3 +23,17 @@ Date.prototype.addHours = function(h) {
    this.setTime(this.getTime() + (h*60*60*1000));
    return this;
 };
+
+let csrftoken = Cookies.get('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
