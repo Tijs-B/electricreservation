9f70ebcc1c1d2e94a70aba414fb0e5270faaa683
Tijs-B <tijs.bergmans@student.kuleuven.be>
2019-05-22 17:48:51 +0200

    production settings

---
+++ electricreservation/settings/development.py
@@ -0,0 +1,10 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '4gtug&+2)-z9#_@l%v8pe4tk3q!6@%v$q91p3m@!*hz2xrc50h'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.233', '192.168.43.23', '192.168.0.101']
---
+++ electricreservation/settings/production.py
@@ -0,0 +1,27 @@
+import json
+
+from django.core.exceptions import ImproperlyConfigured
+
+from .base import *
+
+with open(os.environ.get('MYSITE_CONFIG')) as f:
+    configs = json.loads(f.read())
+
+
+def get_env_var(setting, configs=configs):
+    try:
+        val = configs[setting]
+        if val == 'True':
+            val = True
+        elif val == 'False':
+            val = False
+        return val
+    except KeyError:
+        error_msg = "ImproperlyConfigured: Set {0} environment      variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_env_var("SECRET_KEY")
+DEBUG = False
+
+ALLOWED_HOSTS = ['rpi-tijs.duckdns.org']
