43405c059bd0229dcf7bc5fde54af90921359936
Tijs-B <tijs.bergmans@student.kuleuven.be>
2019-05-22 19:55:51 +0200

    move database config to dev settings

--- electricreservation/settings/development.py
+++ electricreservation/settings/development.py
@@ -8,3 +8,14 @@ SECRET_KEY = '4gtug&+2)-z9#_@l%v8pe4tk3q!6@%v$q91p3m@!*hz2xrc50h'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.233', '192.168.43.23', '192.168.0.101']
+
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
