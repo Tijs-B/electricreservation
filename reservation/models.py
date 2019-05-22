ce01f35b6e5dd770491eecae6bc1b086050d093b
Tijs-B <tijs.bergmans@student.kuleuven.be>
2019-05-22 22:29:17 +0200

    toch terug die receiver

--- reservation/models.py
+++ reservation/models.py
@@ -22,6 +22,17 @@ class Profile(models.Model):
         return f"Profile for {self.user}"
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
 class Car(models.Model):
     name = models.CharField(_("Name"), max_length=200)
 
