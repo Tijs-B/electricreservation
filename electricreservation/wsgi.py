38c4ee416a6338b8665b6ba2656d25ca96b6a8b0
Tijs-B <tijs.be@gmail.com>
2019-05-22 20:53:00 +0100

    wsgi

--- electricreservation/wsgi.py
+++ electricreservation/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electricreservation.settings.production')
+sys.path.append('/home/pi/electricreservation')
+sys.path.append('/home/pi/electricreservation/electricreservation')
 
 application = get_wsgi_application()
